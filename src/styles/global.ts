98d13f9424fae36518ca2277d845202e915b59cd
AlesandraIsla <alesandramempis@gmail.com>
2022-02-15 21:20:39 -0300

    Component header finalizado

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -18,25 +18,27 @@ export const GlobalStyle = createGlobalStyle`
         box-sizing: border-box;
     }
 
-//font-size: 16px padrão desktop
-html { 
-    @media (max-width:1080px) {
-        font-size: 93.75%; //15px
+    html { 
+        @media (max-width:1080px) {
+            font-size: 93.75%; //15px
+        }
+        @media (max-width:720px) {
+            font-size: 875%;
+        }
     }
-    @media (max-width:720px) {
-        font-size: 875%;
-    }
-}
-
-// REM = 1rem = font-size 16px 
-//Boa prática utilizar ele bt img logo icons 
-//se adaptando a cada tela do usuoario
-//percentual vai aumentar e diminuir de acordo com a preferencia do usuario
-
 
     body { 
         background: var(--background);
         -webkit-font-smoothing: antialiased; // font nítida 
+    }
+
+    body, input, textarea, button {
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400;
+    }
+
+    h1, h2, h3, h4, h5, h6, strong {
+        font-weight: 600;
     }
 
     button { 
