ed1215cc44ae6c6a81a9b132ea153047e3823d6a
AlesandraIsla <alesandramempis@gmail.com>
2022-02-18 18:09:08 -0300

    Finalizando a inserção

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -15,9 +15,10 @@ type TransactionInput = Omit<Transaction, 'id' | 'createdAt'>;
 interface TransactionsProviderProps {
     children: ReactNode
 }
+
 interface TransactionsContextData {
     transactions: Transaction[];
-    createTransaction: (transaction: TransactionInput) => void;
+    createTransaction: (transaction: TransactionInput) => Promise<void>;
 
 }
 
@@ -33,8 +34,16 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
             .then(response => setTransactions(response.data.transactions))
     }, []);
 
-    function createTransaction(transaction: TransactionInput) {
-        api.post('/transactions', transaction)
+    async function createTransaction(transactionInput: TransactionInput) {
+        const response = await api.post('/transactions', {
+            ...transactionInput,
+            createdAt: new Date(),
+        })
+        const { transaction } = response.data;
+        setTransactions([
+            ...transactions,
+            transaction,
+        ]);
     }
 
     return (
